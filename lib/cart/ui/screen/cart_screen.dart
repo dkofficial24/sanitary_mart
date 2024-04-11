98412382044a95d69356e3abf997fbd84df3693d
Dinesh Kumar <dkofficial24@gmail.com>
2024-04-11 20:53:46 +0530

    - fix: cart items does not on screen open

--- lib/cart/ui/screen/cart_screen.dart
+++ lib/cart/ui/screen/cart_screen.dart
@@ -7,6 +7,7 @@ import 'package:sanitary_mart/auth/provider/auth_provider.dart';
 import 'package:sanitary_mart/cart/provider/cart_provider.dart';
 import 'package:sanitary_mart/cart/ui/screen/checkout_screen.dart';
 import 'package:sanitary_mart/cart/ui/screen/widget/cart_item_widget.dart';
+import 'package:sanitary_mart/core/provider_state.dart';
 import 'package:sanitary_mart/core/widget/custom_app_bar.dart';
 
 class CartScreen extends StatefulWidget {
@@ -35,92 +36,107 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final cartProvider = Provider.of<CartProvider>(context,listen: false);
-
-    // Calculate subtotal
-    double subtotal = cartProvider.cartItems.fold(
-        0,
-            (previousValue, cartItem) =>
-        previousValue + (cartItem.price * cartItem.quantity));
-
     // double totalDiscount = cartProvider.cartItems.fold(
     //     0,
     //     (previousValue, cartItem) =>
     //         previousValue +
     //         ((cartItem.discountAmount ?? 0) * cartItem.quantity));
 
-    return Scaffold(
-      appBar: const CustomAppBar(
-        title: 'Cart',
-      ),
-      body: subtotal >= 1
-          ? Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Subtotal:',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.grey[700],
-                        ),
+    return Consumer<CartProvider>(builder: (context, cartProvider, child) {
+      double subtotal = cartProvider.cartItems.fold(
+          0,
+              (previousValue, cartItem) =>
+          previousValue + (cartItem.price * cartItem.quantity));
+
+      late Widget widget;
+
+      if (cartProvider.state == ProviderState.loading) {
+        widget = const Center(
+          child: CircularProgressIndicator(),
+        );
+      } else {
+        if (subtotal >= 1) {
+          widget = Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Subtotal:',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.grey[700],
                       ),
-                      Text(
-                        '₹${subtotal.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    ),
+                    Text(
+                      '₹${subtotal.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
-                ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: cartProvider.cartItems.length,
-              itemBuilder: (context, index) {
-                final cartItem = cartProvider.cartItems[index];
-                return CartItemWidget(
-                  cartItem: cartItem,
-                  onRemove: () {
-                    String? uId = getUserId(context);
-                    cartProvider.removeFromCart(uId!, cartItem.productId);
-                  },
-                );
-              },
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: subtotal >= 1
-                  ? () {
-                Get.to(CheckoutScreen(
-                  cartItems: cartProvider.cartItems,
-                ));
-              }
-                  : null,
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
-                // Full width button
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ],
                 ),
               ),
-              child: const Text('Checkout'),
-            ),
-          ),
-        ],
-      )
-          : Center(
-        child: Text(
-          'Cart is empty',
-          style: TextStyle(fontSize: 16.0, color: Colors.grey[600]),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: cartProvider.cartItems.length,
+                  itemBuilder: (context, index) {
+                    final cartItem = cartProvider.cartItems[index];
+                    return CartItemWidget(
+                      cartItem: cartItem,
+                      onRemove: () {
+                        String? uId = getUserId(context);
+                        cartProvider.removeFromCart(uId!, cartItem.productId);
+                      },
+                    );
+                  },
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: subtotal >= 1
+                      ? () {
+                    Get.to(CheckoutScreen(
+                      cartItems: cartProvider.cartItems,
+                    ));
+                  }
+                      : null,
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50),
+                    // Full width button
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: const Text('Checkout'),
+                ),
+              ),
+            ],
+          );
+        } else {
+          widget = emptyState();
+        }
+      }
+
+      return Scaffold(
+        appBar: const CustomAppBar(
+          title: 'Cart',
         ),
+        body: widget,
+      );
+    });
+  }
+
+  Center emptyState() {
+    return Center(
+      child: Text(
+        'Cart is empty',
+        style: TextStyle(fontSize: 16.0, color: Colors.grey[600]),
       ),
     );
   }
