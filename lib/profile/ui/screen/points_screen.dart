ffc99273c1d9877a76a9597587264e8b733d8437
Dinesh <dkofficial24@gmail.com>
2024-06-16 17:22:41 +0530

    - Incentive under dev screen

---
+++ lib/profile/ui/screen/points_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PointsScreen extends StatefulWidget {
+  const PointsScreen({super.key});
+
+  @override
+  State<PointsScreen> createState() => _PointsScreenState();
+}
+
+class _PointsScreenState extends State<PointsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Under developement'),
+      ),
+    );
+  }
+}
